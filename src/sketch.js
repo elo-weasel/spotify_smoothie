c359c965fff05b43e771549aa462c29ee955b2b3
elo-weasel <113067937+elo-weasel@users.noreply.github.com>
2025-04-09 22:10:21 -0700

    adding stuff in

---
+++ src/sketch.js
@@ -0,0 +1,13 @@
+import '../css/style.css';
+import {sketch} from 'p5js-wrapper';
+
+sketch.setup = function(){
+  createCanvas (800, 600);
+}
+
+sketch.draw = function(){
+    background(200)
+    text(document.getElementById("genre1").innerText, width/2, height/2);
+}
+
+
