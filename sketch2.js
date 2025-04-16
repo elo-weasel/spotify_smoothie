9a5fbe6ea060a7887cf28774ee1a08ff437ebd5d
elo-weasel <113067937+elo-weasel@users.noreply.github.com>
2025-04-16 11:42:38 -0700

    color and getting rid of test

--- sketch2.js
+++ sketch2.js
@@ -6,18 +6,12 @@ function preload(){
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
-    x = 0;
-    y = 0;
 }
 
 
 function draw() {
     background(200);
     text(windowWidth + ", " + windowHeight, 50,50);
-
-    ellipse(x,y,10,10);
-    x++;
-    y++;
 }
 
 
